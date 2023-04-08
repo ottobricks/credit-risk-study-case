6a6b4d64ad6a9f8d7a9dd4116bdc11b0a066a783
Otto von Sperling <otto@vonsperling.com.br>
2023-04-08 21:04:15 +0300

    Improve train-test split logic

--- credit-risk/model-experiments/holdout-split-featureframe.py
+++ credit-risk/model-experiments/holdout-split-featureframe.py
@@ -1,5 +1,7 @@
+import argparse
+import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr
+from pyspark.sql.functions import col, expr
 
 
 if __name__ == "__main__":
@@ -7,19 +9,28 @@ if __name__ == "__main__":
     parser.add_argument(
         "--maxdepth",
         type=int,
+        required=True,
         help="Decide which featureframe to run model fit on based on --maxdepth"
+        + " used in the automatic feature engineering step",
+    )
+    parser.add_argument(
+        "--target",
+        type=str,
+        required=True,
+        help="Target column name for ML task"
         + " used in the automatic feature engineering step",
     )
     args = parser.parse_args()
     maxdepth = args.maxdepth
+    target_col = args.target
 
     # Start or fetch active Spark session
     spark = SparkSession.builder.getOrCreate()
 
     # Load featureframes
     dfs_featureframe = spark.read.parquet(
-        f"data/pca-featureframe-maxdepth{maxdepth}.parquet"
-        # f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/pca-featureframe-maxdepth{maxdepth}.parquet"
+        # f"data/pca-featureframe-maxdepth{maxdepth}.parquet"
+        f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/pca-featureframe-maxdepth{maxdepth}.parquet"
     )
     # experiment_featureframe = spark.read.parquet(...)
 
@@ -34,10 +45,13 @@ if __name__ == "__main__":
 
         ]]
         .assign(
-            LOAN_PAID_IN_FULL=lambda x: (x["PAYMENT_STATUS"].str.lower() == "paid").astype("int"),
+            LOAN_NOT_PAID=lambda x: (x["PAYMENT_STATUS"].str.lower() != "paid").astype("int"),
             FIRST_TRAIL_DELAY=lambda x: (x["FIRST_TRAIL_DELAYS"] > 0).astype("int"),
         )
-)
+        .drop(["FIRST_TRAIL_DELAYS", "PAYMENT_STATUS"], axis=1)
+    )
+    assert target_col.lower() in list(map(str.lower, targets.columns)), f"Column --target '{target_col}' not present in featureframe."
+    targets = targets.select("LOAN_ID", col(target_col).alias("label"))
 
     # Join featureframes and targets
     # featureframe = (
@@ -52,46 +66,39 @@ if __name__ == "__main__":
         "stratified_split",
         expr("percent_rank() over (partition by label order by LOAN_ID)")
         # Assumes LOAN_ID is monotonically_increasing to avoid data leak
-    )
+    ).persist()
 
     # Write test dataset to disk
     (
         featureframe.where(
-            "(label = 1 and stratified_split > 0.5)"
+            "(label = 1 and stratified_split >= 0.7)"
             + " or (label = 0 and stratified_split >= 0.8)"
         )
         .write.mode("overwrite")
         .parquet(
-            f"data/test/pca-featureframe-maxdepth{maxdepth}.parquet"
+            # f"data/test/pca-featureframe-maxdepth{maxdepth}.parquet"
             # f"data/test/full-featureframe-maxdepth{maxdepth}.parquet"
-            # f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/test/pca-featureframe-maxdepth{maxdepth}.parquet"
-        )
-    )
-
-    # Write validation dataset to disk
-    (
-        featureframe.where(
-            "(label = 1 and stratified_split between 0.35 and 0.5)"
-            + " or (label = 0 and stratified_split between 0.6 and 0.8)"
-        )
-        .write.mode("overwrite")
-        .parquet(
-            f"data/valid/pca-featureframe-maxdepth{maxdepth}.parquet"
-            # f"data/valid/full-featureframe-maxdepth{maxdepth}.parquet"
-            # f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/valid/pca-featureframe-maxdepth{maxdepth}.parquet"
+            f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/test/pca-featureframe-maxdepth{maxdepth}.parquet"
         )
     )
 
     # Write train dataset to disk
     (
         featureframe.where(
-            "(label = 1 and stratified_split < 0.35)"
-            + " or (label = 0 and stratified_split < 0.6)"
+            "(label = 1 and stratified_split < 0.7)"
+            + " or (label = 0 and stratified_split < 0.8)"
+        )
+        .withColumn(
+            "validation_set",
+            expr(
+                "(label = 1 and stratified_split between 0.5 and 0.7)"
+                + " or (label = 0 and stratified_split between 0.6 and 0.8)"
+            )
         )
         .write.mode("overwrite")
         .parquet(
-            f"data/valid/pca-featureframe-maxdepth{maxdepth}.parquet"
-            # f"data/valid/full-featureframe-maxdepth{maxdepth}.parquet"
-            # f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/valid/pca-featureframe-maxdepth{maxdepth}.parquet"
+            # f"data/train/pca-featureframe-maxdepth{maxdepth}.parquet"
+            # f"data/train/full-featureframe-maxdepth{maxdepth}.parquet"
+            f"s3a://ml-production-fraud-sagemaker-data/otto.sperling/tmp/train/pca-featureframe-maxdepth{maxdepth}.parquet"
         )
     )
